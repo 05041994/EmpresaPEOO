53146d5c6bb5eaa4c5d56072ad41323a44756ad7
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2023-01-24 10:54:36 -0300

    Add files via upload

---
+++ EmpresaPEOO/Empresa/List-SetorWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Empresa
+{
+    /// <summary>
+    /// Lógica interna para List_SetorWindow.xaml
+    /// </summary>
+    public partial class List_SetorWindow : Window
+    {
+        public List_SetorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
